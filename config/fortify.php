82f947c0d8d0f85529606c850bf1436efa5e08aa
Maher El Gamil <maherelgamil@gmail.com>
2026-05-01 18:30:28 +0300

    feat: disable auth system routes (registration, password reset, email verification, 2FA)

--- config/fortify.php
+++ config/fortify.php
@@ -143,15 +143,6 @@ return [
     |
     */
 
-    'features' => [
-        Features::registration(),
-        Features::resetPasswords(),
-        Features::emailVerification(),
-        Features::twoFactorAuthentication([
-            'confirm' => true,
-            'confirmPassword' => true,
-            // 'window' => 0
-        ]),
-    ],
+    'features' => [],
 
 ];
